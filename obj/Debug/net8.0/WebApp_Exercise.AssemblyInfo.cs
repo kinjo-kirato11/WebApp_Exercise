5f884954420c107c692a7e63fad576e79955f9e6
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-05-14 15:54:06 +0900

    演習４

--- obj/Debug/net8.0/WebApp_Exercise.AssemblyInfo.cs
+++ obj/Debug/net8.0/WebApp_Exercise.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApp_Exercise")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ace2aca9ef1a582b5107b96d5b5a0e4f6f16244")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbaab4558fbaeb1f15beb4123c15a0e8e4b511c8")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApp_Exercise")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApp_Exercise")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
