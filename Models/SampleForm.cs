d2524fcf5116a794301614a55a9891cbf1fb525d
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-05-15 12:29:14 +0900

    リスト4-3

---
+++ Models/SampleForm.cs
@@ -0,0 +1,17 @@
+/// <summary>
+/// リスト3-4 
+/// Razor構文の利用
+/// </summary>
+public class SampleForm
+{
+    /// <summary>
+    /// 氏名プロパティ
+    /// </summary>
+    /// <value></value>
+    public string? Name { get; set; }
+    /// <summary>
+    /// 年齢プロパティ
+    /// </summary>
+    /// <value></value>
+    public int Age { get; set; }
+}
