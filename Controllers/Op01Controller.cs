0430d36a0b993983b24c43de3085d76726d355fb
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-05-15 10:17:14 +0900

    オプション演習１

---
+++ Controllers/Op01Controller.cs
@@ -0,0 +1,59 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp_Exercise.Controllers
+{
+
+    /// <summary>
+    /// 演習-04 クエリパラメータを取得するコントローラを実装する
+    /// </summary>
+    [Route("Op01")]
+    public class Op01Controller : Controller
+    {
+        /// <summary>
+        /// クエリパラメータを取得して計算結果を返す
+        /// </summary>
+        /// <param name="opt">計算種別（1:加算、2:減算、3:乗算、4:除算、5:剰余）</param>
+        /// <param name="value1">値1</param>
+        /// <param name="value2">値2</param>
+        /// <returns></returns>
+    [HttpGet("Calc")]
+public IActionResult Calc(int opt, int value1, int value2)
+    {
+        string result;
+
+
+        switch (opt)
+        {
+            case 1:
+                result = $"{value1 + value2}";
+                break;
+
+            case 2:
+                result = $"{value1 - value2}";
+                break;
+
+            case 3:
+                result = $"{value1 * value2}";
+                break;
+
+            case 4:
+                result = $"{value1 / value2}";
+                break;
+
+            case 5:
+                result = $"{value1 % value2}";
+                break;
+
+            default:
+                result = "不明な計算種別です。";
+                break;
+        }
+
+        return Content(value1.ToString() + " " + opt.ToString() + " " + value2.ToString() + " = " + result);
+    }
+}
+}
+
+
+
+
--- Models/Exercise07Form.cs
+++ Models/Exercise07Form.cs
@@ -6,4 +6,5 @@ public class Exercise07Form
 {
     public int Value1 { get; set; }
     public int Value2 { get; set; }
+    public int Opt { get; set; }
 }
