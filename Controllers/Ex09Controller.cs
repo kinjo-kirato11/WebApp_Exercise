1b6fe1537d93ba6903ab3f60ba03dd48cbf26d73
kinjo-kirato11 <2026opnewemp005@fullness.email>
2026-05-18 11:23:22 +0900

    演習１２

--- Controllers/Ex09Controller.cs
+++ Controllers/Ex09Controller.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
 /// <summary>
 /// 演習-09 HTML Formを作成するタグヘルパーを利用する
 /// </summary>
@@ -20,15 +21,23 @@ public class Ex09Controller : Controller
     /// [計算]ボタンクリックアクション
     /// </summary>
     /// <returns></returns>
-    [HttpPost("Result")]
-    public IActionResult Result(Exercise07Form form)
+    /// <summary>
+    /// 計算処理と結果画面表示アクション
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet("Result")]
+    public IActionResult Result()
     {
-        // バリデーションチェック
-        if (!ModelState.IsValid)
+        // TempDataからERxercise07Formを取り出す
+        string? json = (string)TempData["Exercise07Form"]!;
+        if (string.IsNullOrEmpty(json))
         {
-            return View("Enter", form);
+            // TempDataにExercise07Formが無い場合、入力画面表示にリダイレクトする
+            return RedirectToAction("Enter");
         }
-        form.Answer = form.Value1 + form.Value2;
+        // 存在する場合はデシリアライズする
+        var form = JsonSerializer.Deserialize<Exercise07Form>(json);
+        form!.Answer = form.Value1 + form.Value2;
         return View(form);
     }
     /// <summary>
@@ -40,5 +49,20 @@ public class Ex09Controller : Controller
     {
         return RedirectToAction("Enter");
     }
-
+    [HttpPost("Calc")]
+    public IActionResult Calc(Exercise07Form form)
+    {
+        // バリデーションチェック
+        if (!ModelState.IsValid)
+        {
+            // バリデーションエラーの場合入力画面を表示する
+            return View("Enter", form);
+        }
+        // Exercise07Formをシリアライズする
+        var json = JsonSerializer.Serialize(form);
+        // TempDataに登録する
+        TempData["Exercise07Form"] = json;
+        // 計算結果を表示するResultへリダイレクトする
+        return RedirectToAction("Result");
+    }
 }
